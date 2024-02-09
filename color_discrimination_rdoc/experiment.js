fabf8a6b628dd236c81775922d5133d4d4624fea
Logan Bennett <bennett.j.logan@gmail.com>
2024-02-08 16:37:46 -0800

    adding post task feedback block

--- color_discrimination_rdoc/experiment.js
+++ color_discrimination_rdoc/experiment.js
@@ -147,6 +147,30 @@ var testNode = {
   timeline: testTrials,
 };
 
+var postTaskQuestion =
+  "Do you have any comments, concerns, or issues pertaining to this task?";
+
+var postTaskBlock = {
+  type: jsPsychSurveyText,
+  questions: [
+    {
+      prompt: `<h1 class=block-text>${postTaskQuestion}</h1>`,
+      name: postTaskQuestion,
+      required: false,
+      rows: 20,
+      columns: 80,
+    },
+  ],
+  response_ends_trial: true,
+  data: {
+    trial_id: "post_task_feedback",
+  },
+  on_finish: function (data) {
+    data.question = postTaskQuestion;
+    data.response = data.response[postTaskQuestion];
+  },
+};
+
 var fullscreen = {
   type: jsPsychFullscreen,
   fullscreen_mode: true,
@@ -178,6 +202,7 @@ var color_discrimination_rdoc_init = () => {
   color_discrimination_rdoc_experiment.push(fullscreen);
   color_discrimination_rdoc_experiment.push(instructionsBlock);
   color_discrimination_rdoc_experiment.push(testNode);
+  color_discrimination_rdoc_experiment.push(postTaskBlock);
   color_discrimination_rdoc_experiment.push(endBlock);
   color_discrimination_rdoc_experiment.push(exitFullscreen);
 };
--- go_nogo_rdoc/experiment.js
+++ go_nogo_rdoc/experiment.js
@@ -764,6 +764,30 @@ var testNode = {
   },
 };
 
+var postTaskQuestion =
+  "Do you have any comments, concerns, or issues pertaining to this task?";
+
+var postTaskBlock = {
+  type: jsPsychSurveyText,
+  questions: [
+    {
+      prompt: `<h1 class=block-text>${postTaskQuestion}</h1>`,
+      name: postTaskQuestion,
+      required: false,
+      rows: 20,
+      columns: 80,
+    },
+  ],
+  response_ends_trial: true,
+  data: {
+    trial_id: "post_task_feedback",
+  },
+  on_finish: function (data) {
+    data.question = postTaskQuestion;
+    data.response = data.response[postTaskQuestion];
+  },
+};
+
 var fullscreen = {
   type: jsPsychFullscreen,
   fullscreen_mode: true,
@@ -800,6 +824,7 @@ var go_nogo_rdoc_init = () => {
   go_nogo_rdoc_experiment.push(instructionNode);
   go_nogo_rdoc_experiment.push(practiceNode);
   go_nogo_rdoc_experiment.push(testNode);
+  go_nogo_rdoc_experiment.push(postTaskBlock);
   go_nogo_rdoc_experiment.push(endBlock);
   go_nogo_rdoc_experiment.push(exitFullscreen);
 };
