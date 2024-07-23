9513bfd8ed2c6c9f07701c5d7da5a3ad1e7e1ae1
Logan Bennett <bennett.j.logan@gmail.com>
2024-07-23 14:54:40 -0700

    post task battery with plugin

---
+++ post_battery_feedback_rdoc/experiment.js
@@ -0,0 +1,57 @@
+var fullscreen = {
+  type: jsPsychFullscreen,
+  fullscreen_mode: true,
+};
+var exitFullscreen = {
+  type: jsPsychFullscreen,
+  fullscreen_mode: false,
+};
+
+var expID = "post_battery_feedback_rdoc";
+
+var endText = `
+  <div class="centerbox">
+    <p class="center-block-text">Thanks for completing this task!</p>
+    <p class="center-block-text">Press <i>enter</i> to continue.</p>
+  </div>
+`;
+
+var endBlock = {
+  type: jsPsychHtmlKeyboardResponse,
+  data: {
+    trial_id: "end",
+    exp_id: expID,
+    trial_duration: 180000,
+  },
+  trial_duration: 180000,
+  stimulus: endText,
+  choices: ["Enter"],
+};
+
+var post_task_question =
+  "<p><b>Do you have any comments, concerns, or issues pertaining to the battery you just completed?</b></p><p>This screen will advance automatically in 1 minute.</p>";
+
+var text_box = `
+<form>
+    <textarea id='feedback_response' rows="10" cols="100"></textarea>
+</form>
+`;
+var trial = {
+  type: PostBatteryFeedback,
+  data: {
+    trial_id: "post_battery_feedback",
+    trial_duration: 60000,
+  },
+  prompt: post_task_question,
+  html: text_box,
+  response_ends_trial: true,
+  trial_duration: 60000,
+};
+
+post_battery_feedback_rdoc_experiment = [];
+var post_battery_feedback_rdoc_init = () => {
+  post_battery_feedback_rdoc_experiment.push(fullscreen);
+  post_battery_feedback_rdoc_experiment.push(trial);
+  post_battery_feedback_rdoc_experiment.push(endBlock);
+  post_battery_feedback_rdoc_experiment.push(exitFullscreen);
+};
